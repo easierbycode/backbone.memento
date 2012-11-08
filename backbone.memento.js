b9a354110a4ee1bc8043cd533b0ed0c6ffbb22a1
OpenShift guest <navseeker7@gmail.com>
2012-11-08 03:44:25 -0500

    at 40:30 of "Refactoring JavaScript". move serialize behaviors into a module. next step is to refactor logic inside type variable and move it into a helper

--- backbone.memento.js
+++ backbone.memento.js
@@ -1,4 +1,4 @@
-// Backbone.Memento v0.3.0
+// Backbone.Memento v0.4.0
 //
 // Copyright (C)2011 Derick Bailey, Muted Solutions, LLC
 // Distributed Under MIT Liscene
@@ -9,132 +9,148 @@
 // ----------------------------
 // Backbone.Memento
 // ----------------------------
-Backbone.Memento = (function(){
+Backbone.Memento = (function(Backbone, _){
 
   var Memento = function(structure, config) {
+    this.version = "0.4.0";
 
-  this.version = "0.3.0";
+    config = _.extend({ ignore:[] }, config);
 
-  config = _.extend({
-    ignore: []
-  }, config);
+    var serializer = new Serializer(structure, config);
+    var mementoStack = new MementoStack(structure, config, serializer);
 
-  var attributeStack;
-  var type = (structure instanceof Backbone.Model) ? "model" : "collection";
+    this.store = mementoStack.store;
+    this.restore = mementoStack.restore;
+    this.restart = mementoStack.restart;
+  };
 
-  function getAddedAttrDiff(newAttrs, oldAttrs){
-    var removedAttrs = [];
+  var Serializer = function(structure, config) {
+    var type = (structure instanceof Backbone.Model) ? "model" : "collection";
 
-    // guard clause to ensure we have attrs to compare
-    if (!newAttrs || !oldAttrs){
+    function getAddedAttrDiff(newAttrs, oldAttrs){
+      var removedAttrs = [];
+
+      // guard clause to ensure we have attrs to compare
+      if (!newAttrs || !oldAttrs){
+        return removedAttrs;
+      }
+
+      // if the attr is found in the old set but not in
+      // the new set, then it was remove in the new set
+      for (var attr in oldAttrs){
+        if (oldAttrs.hasOwnProperty(attr)){
+          if (!newAttrs.hasOwnProperty(attr)){
+            removedAttrs.push(attr);
+          }
+        }
+      }
+
       return removedAttrs;
     }
 
-    // if the attr is found in the old set but not in
-    // the new set, then it was remove in the new set
-    for (var attr in oldAttrs){
-      if (oldAttrs.hasOwnProperty(attr)){
-        if (!newAttrs.hasOwnProperty(attr)){
-          removedAttrs.push(attr);
+    function removeAttributes(structure, attrsToRemove){
+      for (var index in attrsToRemove){
+        var attr = attrsToRemove[index];
+        if (type === "model"){
+          structure.unset(attr);
+        } else {
+          structure.remove(attr);
         }
       }
     }
 
-    return removedAttrs;
-  }
+    function dropIgnored(attrs, restoreConfig){
+      attrs = _.clone(attrs);
+      if (restoreConfig.hasOwnProperty("ignore") && restoreConfig.ignore.length > 0){
+        for(var index in restoreConfig.ignore){
+          var ignore = restoreConfig.ignore[index];
+          delete attrs[ignore];
+        }
+      }
+      return attrs;
+    }
 
-  function removeAttributes(structure, attrsToRemove){
-    for (var index in attrsToRemove){
-      var attr = attrsToRemove[index];
+    function restoreState(previousState, restoreConfig){
+      oldAttrs = dropIgnored(previousState, restoreConfig);
+
+      //get the current state
+      var currentAttrs = structure.toJSON();
+      currentAttrs = dropIgnored(currentAttrs, restoreConfig);
+
+      //handle removing attributes that were added
+      var removedAttrs = getAddedAttrDiff(oldAttrs, currentAttrs);
+      removeAttributes(structure, removedAttrs);
+
+      //restore the previous state
       if (type === "model"){
-        structure.unset(attr);
+        structure.set(oldAttrs);
       } else {
-        structure.remove(attr);
+        structure.reset(oldAttrs);
       }
     }
-  }
 
-  function restoreState(last, restoreConfig){
-    //get the previous state
-    var oldAttrs = attributeStack[last];
-    if (oldAttrs === undefined){ return; }
+    this.serialize = function() {
+      var attrs = structure.toJSON();
+      attrs = dropIgnored(attrs, config);
 
-    oldAttrs = dropIgnored(oldAttrs, restoreConfig);
-
-    //get the current state
-    var currentAttrs = structure.toJSON();
-    currentAttrs = dropIgnored(currentAttrs, restoreConfig);
-
-    //handle removing attributes that were added
-    var removedAttrs = getAddedAttrDiff(oldAttrs, currentAttrs);
-    removeAttributes(structure, removedAttrs);
-
-    //restore the previous state
-    if (type === "model"){
-      structure.set(oldAttrs);
-    } else {
-      structure.reset(oldAttrs);
+      return attrs;
     }
 
-    //destroy the no-longer-current state
-    delete attributeStack[last];
-  }
-
-  function dropIgnored(attrs, restoreConfig){
-    attrs = _.clone(attrs);
-    if (restoreConfig.hasOwnProperty("ignore") && restoreConfig.ignore.length > 0){
-      for(var index in restoreConfig.ignore){
-        var ignore = restoreConfig.ignore[index];
-        delete attrs[ignore];
-      }
+    this.deserialize = function(previousState, restoreConfig) {
+      restoreState(previousState, restoreConfig);
     }
-    return attrs;
-  }
-
-  function initialize(){
-    attributeStack = [];
-  }
-
-  this.store = function(){
-    var attrs = structure.toJSON();
-
-    attrs = dropIgnored(attrs, config);
-
-    attributeStack.push(attrs);
-  }
-  
-  this.restore = function(restoreConfig){
-    if (restoreConfig === undefined){
-      restoreConfig = _.clone(config);
-    }
-
-    var last = attributeStack.length-1;
-    if (last < 0){
-      return null;
-    }
-
-    restoreState(last, restoreConfig);
-  }
-
-  this.restart = function(){
-    if(attributeStack.length === 0){
-      return null;
-    }
-    restoreState(0, config);
-    // restoreState deleted item 0, but really 
-    // we should be starting from scratch.
-    initialize();
-  }
-
-  // For backwards compatibility, collections cannot have a property
-  // named reset for conflict reasons.
-  if (type === "model"){
-    this.reset = this.restart;
-  }
-
-  initialize();
   };
-  
+
+  var MementoStack = function(structure, config, serializer) {
+
+    var attributeStack;
+
+    function initialize(){
+      attributeStack = [];
+    }
+
+    this.store = function(){
+      var attrs = serializer.serialize();
+      attributeStack.push(attrs);
+    }
+
+    this.restore = function(restoreConfig){
+      if (restoreConfig === undefined){
+        restoreConfig = _.clone(config);
+      }
+
+      var last = attributeStack.length-1;
+      if (last < 0){
+        return null;
+      }
+
+      restoreState(last, restoreConfig);
+    }
+
+    var restoreState = function(last, restoreConfig) {
+      //get the previous state
+      var oldAttrs = attributeStack[last];
+      if (oldAttrs === undefined){ return; }
+
+      serializer.deserialize(oldAttrs, restoreConfig);
+
+      //destroy the no-longer-current state
+      delete attributeStack[last];
+    }
+
+    this.restart = function(){
+      if(attributeStack.length === 0){
+        return null;
+      }
+      restoreState(0, config);
+      // restoreState deleted item 0, but really
+      // we should be starting from scratch.
+      initialize();
+    }
+
+    initialize();
+  };
+
   return Memento;
-  
-})();
+
+})(Backbone, _);
